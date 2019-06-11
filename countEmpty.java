2af884a648cdb986d368e907edaca516d87f2b9c
ASGEDOM <49059745+askprograming@users.noreply.github.com>
2019-06-11 10:57:53 -0700

    Update countEmpty.java

--- countEmpty.java
+++ countEmpty.java
@@ -3,7 +3,7 @@
  * itself). For non-empty trees, your method(s) should count the total number 
  * of empty branches among the nodes of the tree. A leaf node has two empty 
  * branches. A node with one non-empty child has one empty branch. A node with 
- * two non-empty children (a full branch) has no empty branches.
+ * two non-empty children has no empty branches.
  */
 
 public int countEmpty() {
